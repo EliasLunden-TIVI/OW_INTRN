994a25502ea35a56a4d35df660cebd6f25c2ae67
Pavlo Polish <64736874+PlaguePolski@users.noreply.github.com>
2025-01-30 13:15:45 +0200

    Some comments

--- Java/scripts.java
+++ Java/scripts.java
@@ -3,3 +3,6 @@ document.getElementById("startgame").addEventListener("click", function() {
     document.getElementById("Visual").classList.remove("hidden"); // Show game content
     document.getElementById("Choices").classList.remove("hidden"); // Show game content
 });
+
+// Actually, I not sure is this even Java. This code looks more like JavaScript. And in web developming JavaScript is used for client-side scripting. Java is more used for back-end server side. 
+// Yeah, Java and JavaScript is a different language actually, that not related to each other.
